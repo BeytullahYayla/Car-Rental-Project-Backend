81d3d9e60445573ee014df5e1e81b49287952020
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2021-10-08 12:00:07 +0300

    Exception Extensions And Middleware Refactored

--- Core/Extentions/ErrorDetails.cs
+++ Core/Extentions/ErrorDetails.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace Core.Extentions
 {
-    class ErrorDetails
+   public class ErrorDetails
     {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        
+    }
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        public IEnumerable<ValidationFailure> Errors { get; set; }
     }
 }
--- Core/Extentions/ExceptionMiddlewareExtensions.cs
+++ Core/Extentions/ExceptionMiddlewareExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Core.Extentions
 {
-    class ExceptionMiddlewareExtensions
+    public static class ExceptionMiddlewareExtensions
     {
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
     }
 }
